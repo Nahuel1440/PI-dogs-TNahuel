cd1a563c7500684ebf5bc5ca3e50bad6c105f081
José Nahuel Toloza <nahueltoloza014@gmail.com>
2022-06-07 19:45:05 -0300

    db.js: Add associations

--- api/src/db.js
+++ api/src/db.js
@@ -41,10 +41,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { race, temper } = sequelize.models;
+const { Race, Temper } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Race.belongsToMany(Temper, { through: "raceTemper" });
+Temper.belongsToMany(Race, { through: "raceTemper" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
