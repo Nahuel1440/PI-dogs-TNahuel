e152a0cf7fe8aa8f62d231523441549f37affec8
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-06-07 22:54:56 -0300

    rename the model race to breed

--- api/src/db.js
+++ api/src/db.js
@@ -41,12 +41,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Race, Temper } = sequelize.models;
+const { Breed, Temper } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Race.belongsToMany(Temper, { through: "raceTemper" });
-Temper.belongsToMany(Race, { through: "raceTemper" });
+Breed.belongsToMany(Temper, { through: "breedTemper" });
+Temper.belongsToMany(Breed, { through: "breedTemper" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
