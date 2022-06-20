977218afc762b69ed9318886e430ec607c82bdbc
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-06-20 00:57:41 -0300

    Home.js: Finished

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,58 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import img2 from "../img/img2.jpg";
+import img3 from "../img/img3.jpg";
 import img4 from "../img/img4.jpg";
-
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <Section style={{ backgroundImage: `url(${img2})` }}>
-        <div style={{ zIndex: "1" }}>
+      <Section id="1" style={{ backgroundImage: `url(${img2})` }}>
+        <div style={{ zIndex: 1 }}>
           <header>
-            <H1>Welcome the dogs page</H1>
+            <h1>Welcome the dogs page</h1>
           </header>
         </div>
+        <a href="#2" className="btnMove">
+          <button>▽</button>
+        </a>
       </Section>
-      <Section style={{ backgroundImage: `url(${img4})` }}></Section>
-      <Section style={{ backgroundImage: `url(${img2})` }}></Section>
+      <Section id="2" style={{ backgroundImage: `url(${img4})` }}>
+        <IntroConteiner>
+          <h2>Search dog breeds</h2>
+          <p>
+            You can search for all available dog breeds in our database. This
+            tool can help you find your ideal dog breed, based on its
+            temperament and size.
+          </p>
+          <Link to="/search" onClick={() => window.scroll(0, 0)}>
+            <button>Search</button>
+          </Link>
+        </IntroConteiner>
+        <a href="#3" className="btnMove">
+          <button>▽</button>
+        </a>
+      </Section>
+      <Section id="3" style={{ backgroundImage: `url(${img3})` }}>
+        <IntroConteiner>
+          <h2>Create your dog breed</h2>
+          <p>
+            You can have fun creating your own dog breed, or if you think there
+            is something missing from our database, you can add it here. Then
+            you can see it in the search section. Try it!
+          </p>
+          <Link to="/create" onClick={() => window.scroll(0, 0)}>
+            <button>Create</button>
+          </Link>
+        </IntroConteiner>
+        <a href="#1" className="btnMove">
+          <button>△</button>
+        </a>
+      </Section>
     </>
   );
 }
+
 const fadeIn = keyframes`
     0%{
         opacity: 10;
@@ -26,19 +61,21 @@ const fadeIn = keyframes`
         opacity: 0.2;
     }
 `;
+
 const Section = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   background-position: center;
   background-size: cover;
   background-attachment: fixed;
-  background-repeat: no-repeat;
+  /* background-repeat: no-repeat; */
   height: 100%;
   min-height: 100vh;
   position: relative;
-  overflow: hidden;
-  border-top: 0;
+  /* overflow: hidden;
+  border-top: 0; */
   @media screen and (max-width: 700px) {
     background-attachment: scroll;
   }
@@ -54,9 +91,47 @@ const Section = styled.section`
     z-index: 0;
     animation: ${fadeIn} 5s ease-out;
   }
+  h1 {
+    font: bold 300% cursive;
+    text-align: center;
+    margin-bottom: 90px;
+  }
+  .btnMove {
+    position: absolute;
+    top: 90%;
+    @media screen and (max-width: 700px) {
+      top: 80%;
+    }
+    button {
+      width: 40px;
+      height: 40px;
+      font-size: 20px;
+      color: white;
+      background-color: rgba(255, 255, 255, 0.1);
+      border: none;
+      border-radius: 50px;
+      cursor: pointer;
+      &:hover {
+        background-color: transparent;
+        border: 1px solid white;
+      }
+    }
+  }
 `;
-const H1 = styled.h1`
+const IntroConteiner = styled.article`
+  z-index: 2;
   text-align: center;
-  margin-bottom: 90px;
-  font-size: 40px;
+  width: 70%;
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 15px 50px 30px 50px;
+  button {
+    background-color: transparent;
+    color: white;
+    border: 1px solid white;
+    padding: 5px 20px 5px 20px;
+    cursor: pointer;
+    &:hover {
+      background-color: black;
+    }
+  }
 `;
