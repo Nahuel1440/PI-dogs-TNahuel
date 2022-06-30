51c74d29443b99dc1ffbe1eb5bc36c4389fcb337
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-06-30 00:02:50 -0300

    upTempToDb.js: Bug fixed

--- api/src/helpers/upTempToDb.js
+++ api/src/helpers/upTempToDb.js
@@ -2,9 +2,12 @@ const tempersApi = require("./tempersApi");
 
 const upTempToDb = async (Temperament) => {
   try {
-    const arrTempers = await tempersApi();
-    arrTempers.sort((a, b) => (a.name > b.name ? 1 : -1));
-    await Temperament.bulkCreate(arrTempers);
+    const temperaments = await Temperament.findAll();
+    if (temperaments.length === 0) {
+      const arrTempers = await tempersApi();
+      arrTempers.sort((a, b) => (a.name > b.name ? 1 : -1));
+      await Temperament.bulkCreate(arrTempers);
+    }
   } catch (err) {
     // console.log("Error: No se pudo cargar los temperamentos de la api a la db");
     console.log(err.message);
