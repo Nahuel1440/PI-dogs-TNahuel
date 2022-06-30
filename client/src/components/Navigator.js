40807542bc4103101976af263e0ad179b262011a
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-06-30 15:41:56 -0300

    Navigator.js: finished

--- client/src/components/Navigator.js
+++ client/src/components/Navigator.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import arrowLeft from "../img/arrow-left-filter.png";
+import arrowRight from "../img/arrow-right-filter.png";
 
 const arrPageCreator = (currentPage, cantPages) => {
   const arr = [],
@@ -44,8 +46,11 @@ export default function Navigator({ currentPage, setCurrentPage, loading }) {
         <ul>
           {cantPages > 0 && currentPage > 0 ? (
             <li key={"prev"}>
-              <button onClick={() => handlePage(currentPage - 1)}>
-                {"<Prev"}
+              <button
+                className={"prev"}
+                onClick={() => handlePage(currentPage - 1)}
+              >
+                <img src={arrowLeft} alt="" />
               </button>
             </li>
           ) : null}
@@ -65,8 +70,11 @@ export default function Navigator({ currentPage, setCurrentPage, loading }) {
           ))}
           {cantPages > 0 && currentPage < cantPages - 1 ? (
             <li key={"next"}>
-              <button onClick={() => handlePage(currentPage + 1)}>
-                {"Next>"}
+              <button
+                className={"next"}
+                onClick={() => handlePage(currentPage + 1)}
+              >
+                <img src={arrowRight} alt="" />
               </button>
             </li>
           ) : null}
@@ -83,17 +91,6 @@ const Conteiner = styled.div`
     padding: 0px;
     display: inline-block;
     margin-top: 45px;
-    .active {
-      background-color: rgba(255, 255, 255, 0.1);
-      pointer-events: none;
-      cursor: default;
-      font-weight: 600;
-    }
-    .ellipsis {
-      font-weight: 600;
-      cursor: default;
-      pointer-events: none;
-    }
     li {
       display: inline;
     }
@@ -105,13 +102,44 @@ const Conteiner = styled.div`
       font-size: 15px;
       font-family: sans-serif;
       cursor: pointer;
-      &:hover {
-        background-color: rgba(255, 255, 255, 0.1);
-      }
-      @media screen and (max-width: 700px) {
+      @media screen and (min-width: 701px) {
         &:hover {
-          background-color: transparent;
+          background-color: rgba(255, 255, 255, 0.1);
         }
+      }
+    }
+    .active {
+      background-color: rgba(255, 255, 255, 0.1);
+      pointer-events: none;
+      cursor: default;
+      font-weight: 600;
+    }
+    .ellipsis {
+      font-size: 20px;
+      font-weight: 200px;
+      cursor: default;
+      pointer-events: none;
+      @media screen and (max-width: 700px) {
+        padding-left: 0;
+        padding-right: 0;
+      }
+    }
+    .prev {
+      margin-right: 10px;
+      @media screen and (max-width: 700px) {
+        margin-right: 2px;
+      }
+      img {
+        height: 12px;
+      }
+    }
+    .next {
+      margin-left: 10px;
+      @media screen and (max-width: 700px) {
+        margin-left: 2px;
+      }
+      img {
+        height: 12px;
       }
     }
   }
